f3437f39b78c547e68c359b0b921e2701fb64da5
SmpManeth <smpmaneth@gmail.com>
2022-05-24 14:11:58 +0530

    first commit

---
+++ app/includes/header.php
@@ -0,0 +1,11 @@
+    <div class="navbar shadow-sm" style="background-color:#FFFFFF ;">
+        <div>
+            <p class="text-muted ps-4 pt-2 fw-bold head-text">
+                Deals
+            </p>
+
+        </div>
+    </div>
+    <div class="side-bar fixed-top">
+
+    </div>
---
+++ login.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Login</title>
+</head>
+
+<body>
+
+    <section class="vh-100">
+        <div class="container-fluid h-custom">
+
+            <div class="row d-flex justify-content-center align-items-center h-100">
+
+                <div class="col-md-8 col-lg-6 col-xl-4 ">
+                    <div class="company-logo">
+                        <img src="https://www.sterlinglanka.com/wp-content/uploads/2019/08/rrrr.png" alt="company logo">
+                    </div>
+                    <form>
+
+
+                        <div class="divider d-flex align-items-center my-4">
+                            <p class="text-center fw-bold mx-3 mb-0 h4">Login</p>
+                        </div>
+
+                        <!-- Email input -->
+                        <div class="form-outline mb-4">
+                            <input type="email" id="form3Example3" class="form-control form-control-lg"
+                                placeholder="Enter a valid email address" />
+                            <label class="form-label" for="form3Example3">Email address</label>
+                        </div>
+
+                        <!-- Password input -->
+                        <div class="form-outline mb-3">
+                            <input type="password" id="form3Example4" class="form-control form-control-lg"
+                                placeholder="Enter password" />
+                            <label class="form-label" for="form3Example4">Password</label>
+                        </div>
+
+                        <div class="d-flex justify-content-between align-items-center">
+                            <!-- Checkbox -->
+                            <div class="form-check mb-0">
+                                <input class="form-check-input me-2" type="checkbox" value="" id="form2Example3" />
+                                <label class="form-check-label" for="form2Example3">
+                                    Remember me
+                                </label>
+                            </div>
+
+                        </div>
+
+                        <div class="text-center  mt-5 pt-2">
+                            <button type="button" class="btn btn-primary btn-lg centered"
+                                style="padding-left: 2.5rem; padding-right: 2.5rem;">Login</button>
+                        </div>
+
+                    </form>
+                </div>
+            </div>
+        </div>
+
+    </section>
+    <script src="https://kit.fontawesome.com/27dd36351b.js" crossorigin="anonymous"></script>
+</body>
+
+</html>
