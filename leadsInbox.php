e16b6a4db3971dea25449b97322760443e8cf841
Dasun Sucharith <sucharith.dasun@gmail.com>
2022-05-24 15:48:09 +0530

    new

--- leadsInbox.php
+++ leadsInbox.php
@@ -17,12 +17,12 @@
     <?php include("app/includes/header.php") ?>
 
     <div class="d-flex align-items-start sidenav-menu">
-        <div class="nav flex-column nav-pills me-3" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+        <div class="nav flex-column nav-pills me-3 px-2 sidenav-item" id="v-pills-tab" role="tablist" aria-orientation="vertical">
             <button class="nav-link active" id="v-pills-home-tab" data-bs-toggle="pill" data-bs-target="#v-pills-home" type="button" role="tab" aria-controls="v-pills-home" aria-selected="true">
                 Lead Inbox
             </button>
         </div>
-        <div class="tab-content" id="v-pills-tabContent">
+        <div class="tab-content sidenav-item" id="v-pills-tabContent">
             <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab" tabindex="0">
                 <table class="table">
                     <thead>
