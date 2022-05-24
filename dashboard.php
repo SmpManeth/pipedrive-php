86b1632807a62ab6177b8d2bb60e499860377bee
SmpManeth <smpmaneth@gmail.com>
2022-05-24 16:59:35 +0530

    logout code added

--- dashboard.php
+++ dashboard.php
@@ -12,11 +12,12 @@
 </head>
 
 <body style="background-color:#F7F7F7 ">
-
     <!-- header and Sidebar -->
     <?php include("app/includes/header.php") ?>
 
+
     <script src="https://kit.fontawesome.com/27dd36351b.js" crossorigin="anonymous"></script>
+   <script src="assets/js/scripts.js"></script>
 </body>
 
 </html>
---
+++ logout.php
@@ -0,0 +1,17 @@
+<?php
+//importing baseurl variable in the path.php
+include("path.php");
+
+session_start();
+//unset all values taken by the user.
+unset($_SESSION['id']);
+unset($_SESSION['username']);
+unset($_SESSION['admin']);
+unset($_SESSION['message']);
+unset($_SESSION['type']);
+
+
+session_destroy();
+
+//redirect to the homepage
+header('location: ' . $BASE_URL . 'login.php');
