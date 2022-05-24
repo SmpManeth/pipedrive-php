8a46a7231ee922f9f38597566690db3a91e2d6cf
SmpManeth <smpmaneth@gmail.com>
2022-05-24 14:43:20 +0530

    home link added

--- app/includes/header.php
+++ app/includes/header.php
@@ -1,11 +1,21 @@
+    <!-- header -->
     <div class="navbar shadow-sm" style="background-color:#FFFFFF ;">
         <div>
-            <p class="text-muted ps-4 pt-2 fw-bold head-text">
-                Deals
+            <p class=" ps-4 pt-2 fw-bold head-text">
+                <a class="text-decoration-none text-muted" href="dashboard.php">Deals</a>
             </p>
 
         </div>
+        <div class="search">
+            <input type="checkbox" id="trigger" class="search__checkbox" />
+            <label class="search__label-init" for="trigger"></label>
+            <label class="search__label-active" for="trigger"></label>
+            <div class="search__border"></div>
+            <input type="text" class="search__input" />
+            <div class="search__close"></div>
+        </div>
     </div>
+    <!-- side bar -->
     <div class="side-bar fixed-top">
 
     </div>
