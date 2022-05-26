f4e2c83fb7e53504ebb4aec70be12482f3975821
SmpManeth <smpmaneth@gmail.com>
2022-05-26 10:00:33 +0530

    updated

--- dashboard.php
+++ dashboard.php
@@ -2,17 +2,12 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- CSS only -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-    <link rel="stylesheet" href="assets/css/style.css">
+    <?php include("app/includes/head.php") ?>
     <title>Pipeline name + Dashboard</title>
 </head>
 
 <body style="background-color:#F7F7F7 ">
-    <!-- header and Sidebar -->
+
     <!-- header and Sidebar -->
     <?php include("app/includes/header.php") ?>
     <div class="d-flex sidenav-menu">
