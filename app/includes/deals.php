99db8dd863cc681912717e2cf2f1ede5275bb0da
SmpManeth <smpmaneth@gmail.com>
2022-06-09 13:58:53 +0530

    Editabale Deals

--- app/includes/deals.php
+++ app/includes/deals.php
@@ -1,30 +1,35 @@
 <?php
-$deals = selectAll('deal');
-$stages = selectAll('stages');
+$deals = selectAll('tbl_task');
+$stages = selectAll('tbl_status');
 $currencies = selectAll('currency');
 $mob_typr = selectAll('phone_type')
 // dd($currencies);
 ?>
 
+
+
+
 <div class="tab-pane fade show active" id="v-pills-deals" role="tabpanel" aria-labelledby="v-pills-home-tab" tabindex="0">
    <!-- Modal -->
-   <div class="modal fade" id="exampleModal" tabindex="ria-hidden="true">
+   <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
       <div class="modal-dialog">
          <div class="modal-content">
             <div class="modal-header">
                <h5 class="modal-title" id="exampleModalLabel">Add Deal</h5>
                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-               </div>
+            </div>
             <div class="modal-body p-0 ps-3 pe-3">
                <form action="dashboard.php" method="get">
                   <div class="row">
                      <div class="col-6 left-col-deal p-2 ps-3">
                         <div>
                            <div class="model-item text-muted">
-                              <p class="" > Contact Person</p>
+                              <p class="">Contact Person</p>
                               <i class="fa fa-user icon"></i>
                               <input class="input-field input-text" name="Contact_person_Name" type="text">
+
                            </div>
+
                            <div class="model-item text-muted">
                               <p class="">Organization</p>
                               <i class="fa fa-building icon"></i>
@@ -32,6 +37,7 @@ $mob_typr = selectAll('phone_type')
                            </div>
                            <div class="model-item text-muted">
                               <p class="">Title</p>
+
                               <input class="input-field" name="title" type="text">
                            </div>
                            <div class="model-item text-muted">
@@ -43,9 +49,12 @@ $mob_typr = selectAll('phone_type')
                                        <?php echo $currency['currency_name'] ?></option>
                                  <?php
                                  }
+
+
                                  ?>
                               </select>
                            </div>
+
                            <div class="model-item text-muted">
                               <p class="">Pipeline</p>
                               <select class="input-field" name="pipeline" id="" style="height: 27px; padding: 0px;">
@@ -81,7 +90,7 @@ $mob_typr = selectAll('phone_type')
                      <div class="col-6 p-2 ps-3">
 
                         <div class="model-item text-muted">
-                           <h11 class="modal-title" style="color:black"> PERSON </h11>
+                           <h9 class="modal-title" style="color:black"> Person </h9>
                            <p class="">Phone</p>
                            <input class="input-field" type="text" name="value" maxlength="10" style="width: 150px;">
                            <select class="input-field" name="phone category" id4er="" style="width: 120px; height: 27px; padding: 0px;">
@@ -90,28 +99,28 @@ $mob_typr = selectAll('phone_type')
                                     <?php echo $phone_type['type_name'] ?></option>
                               <?php   } ?>
                            </select>
-                           <!-- <i class="fa fa-trash" aria-hidden="true"></i> -->
                            <br>
                            <!-- <input class="input-field" type="text" name="Add Phone"> -->
-                           <div class="input_fields_wrap" style="margin-top:6px;">
-                           </div>
                            <a class="add_field_button text-decoration-none  h6"> + Add Phone </a>
+                           <div class="input_fields_wrap">
+                           </div>
                         </div>
 
                         <div class="model-item text-muted">
                            <p class="">Email</p>
                            <input class="input-field" type="text" name="email">
                            <br>
-                           <div class="input_wrap" style="margin-top:6px;">
-                           </div>
                            <a class="add_f_button text-decoration-none  h6"> + Add Email </a>
+                           <div class="input_wrap">
+                           </div>
+
                         </div>
                      </div>
                   </div>
 
                   <div class="modal-footer">
 
-                     <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Close</button>
+                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                      <button type="submit" value="add_deal" id="deal_submit" name="submit_deal" class="btn btn-primary">Save changes</button>
                   </div>
                </form>
@@ -139,16 +148,15 @@ $mob_typr = selectAll('phone_type')
             <button type="button" class="btn btn-primary-deal" data-bs-toggle="modal" data-bs-target="#exampleModal">
                + Deal
             </button>
-<<<<<<< HEAD
 
 
 
 
-=======
->>>>>>> 44f9a97e68027103880c50baf0d7b3f2fa985289
          </div>
          <div>
+
          </div>
+
          <div class="lkr_tag">
             <p>
                LKR 26,100
@@ -172,77 +180,50 @@ $mob_typr = selectAll('phone_type')
                <li><a class="dropdown-item" href="#">+ Edit Pipline</a></li>
             </ul>
          </div>
-         <div class="group_2">
-            <button class="btn btn-primary-1" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+         <!--everyone dropdown list start-->
+         <div class="group_2 dropdown">
+            <button type="button" onclick="myFunction1()" class="btn btn-primary-every">
                Everyone
             </button>
-            <ul class="dropdown-menu">
-               <form class="form-inline d-flex justify-content-center md-form form-sm active-pink active-pink-2 mt-2">
-                  <i class="fas fa-search" aria-hidden="true"></i>
-                  <input class="form-control form-control-sm ml-3 w-75" type="text" placeholder="Search" aria-label="Search">
-               </form>
-               <hr class="dropdown-divider">
-               </li>
+            <div id="myDropdown1" class="dropdown-content1 shadow-sm">
 
-            <div class="dropdown2">
-               <button onclick="myFunction1()" class="dropbtn user everyone">Everyone<i class='fas fa-caret-square-down'></i></button>
-               <div id="myDropdown1" class="dropdown-content1 shadow-sm">
+               <!--everyone button edit start dhanushka -->
+               <div class="nav everyone" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+                  <div class="tab">
+                     <button class="tablinks" onclick="openCity(event, 'London')">Favorites</button>
+                     <button class="tablinks" onclick="openCity(event, 'Paris')">owners</button>
+                     <button class="tablinks" onclick="openCity(event, 'Tokyo')">Filters</button>
+                  </div>
 
-                  <!--everyone button edit start dhanushka -->
-                  <div class="nav everyone" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-                     <div class="tab">
-                        <button class="tablinks" onclick="openCity(event, 'London')">Favorites</button>
-                        <button class="tablinks" onclick="openCity(event, 'Paris')">owners</button>
-                        <button class="tablinks" onclick="openCity(event, 'Tokyo')">Filters</button>
-                     </div>
-                    <!-- Tab content -->
-                     <div id="London" class="tabcontent">
-                        <h3>Favorites</h3>
-                        <h6>Owners</h6>
-                        <a href="#">add new filter</a>
-                     </div>
-                     <div id="Paris" class="tabcontent">
-                        <h3>Owners</h3>
-                        <h6>Everyone</h6>
-                        <a href="#">add new filter</a>
-                     </div>
-                     <div id="Tokyo" class="tabcontent">
-                        <h3>Filters</h3>
-                        <a href="#">All deleted deals</a>
-                        <a href="#">All lost deals</a>
-                        <a href="#">All open deals</a>
-                        <a href="#">All won deals</a>
-                        <a href="#">More than 3 months old deals</a>
-                        <a href="#">Rotten deals</a>
-                     </div>
+                  <!-- Tab content -->
+                  <div id="London" class="tabcontent">
+                     <h3>Favorites</h3>
+                     <h6>Owners</h6>
+                     <a href="#">add new filter</a>
+                  </div>
+
+                  <div id="Paris" class="tabcontent">
+                     <h3>Owners</h3>
+                     <h6>Everyone</h6>
+                     <a href="#">add new filter</a>
+                  </div>
+
+                  <div id="Tokyo" class="tabcontent">
+                     <h3>Filters</h3>
+                     <a href="#">All deleted deals</a>
+                     <a href="#">All lost deals</a>
+                     <a href="#">All open deals</a>
+                     <a href="#">All won deals</a>
+                     <a href="#">More than 3 months old deals</a>
+                     <a href="#">Rotten deals</a>
                   </div>
                </div>
-<<<<<<< HEAD
                <!--everyone button edit start dhanushka -->
             </div>
-=======
->>>>>>> 44f9a97e68027103880c50baf0d7b3f2fa985289
 
-               <div id="Favorit" class="tabcontent">
-                  <span onclick="this.parentElement.style.display='none'" class="topright">&times</span>
-                  <h3>London</h3>
-                  <p>London is the capital city of England.</p>
-               </div>
 
-               <div id="Owners" class="tabcontent">
-                  <span onclick="this.parentElement.style.display='none'" class="topright">&times</span>
-                  <h3>Paris</h3>
-                  <p>Paris is the capital of France.</p>
-               </div>
-
-               <div id="Filters" class="tabcontent">
-                  <span onclick="this.parentElement.style.display='none'" class="topright">&times</span>
-                  <h3>Tokyo</h3>
-                  <p>Tokyo is the capital of Japan.</p>
-               </div>
-
-            </ul>
          </div>
+         <!--everyone dropdown list end-->
 
 
       </div>
@@ -253,14 +234,16 @@ $mob_typr = selectAll('phone_type')
 </div>
 
 <!-- stages and cards -->
-<div class="d-flex flex-row stage-row " >
+<div class="d-flex flex-row stage-row ">
    <!-- stage cards -->
-   <?php foreach ($stages as $key => $stage) { ?>
+
+   <?php foreach ($stages as $key => $tbl_status) { ?>
       <div class="p-2 stage">
+
          <!-- stage heading -->
          <div class="stage-box">
             <div class="stage-head-card mb-2">
-               <p class="fw-bold stage-name ms-3 mt-2 mt-0 me-0 deal-topic"><?php echo $stage['stage_name'] ?></p>
+               <p class="fw-bold stage-name ms-3 mt-2 mt-0 me-0 deal-topic"><?php echo $tbl_status['status_name'] ?></p>
                <div class="d-flex">
                   <p class="text-muted stage-name ms-3 mt-2 mt-0 me-0 deal-p">LKR 23,00000000 :</p>
                   <p class="text-muted stage-name ms-1 mt-2 mt-0 me-0 deal-p">2 Deals</p>
@@ -274,16 +257,15 @@ $mob_typr = selectAll('phone_type')
                </svg>
             </div>
          </div>
+
          <!-- stage card -->
          <div class="deal-stage-col ">
-
-
             <!-- single deal caed -->
-            <?php foreach ($deals as $key => $deal) {
-               if ($stage['stage_id'] == $deal['pipeline_stage']) { ?>
-                  <div class="d-flex flex-column deals-box shadow-sm" draggable="true" >
+            <?php foreach ($deals as $key => $tbl_task) {
+               if ($tbl_status['id'] == $tbl_task['status_id']) { ?>
+                  <div class="d-flex flex-column deals-box shadow-sm" draggable="true">
 
-                     <p id="deal-name" class=" m-0"><?php echo $deal['title'] ?></p>
+                     <p id="deal-name" class=" m-0"><?php echo $tbl_task['title'] ?></p>
                      <p id="deal-organization" class="text-muted m-0">Sterling</p>
                      <div class="d-flex  pt-1">
                         <i class="fa fa-user icon-stage me-1"></i>
@@ -302,33 +284,14 @@ $mob_typr = selectAll('phone_type')
          </div>
 
       </div>
-      <script>
-         function openCity(evt, cityName) {
-            var i, tabcontent, tablinks;
-            tabcontent = document.getElementsByClassName("tabcontent");
-            for (i = 0; i < tabcontent.length; i++) {
-               tabcontent[i].style.display = "none";
-            }
-            tablinks = document.getElementsByClassName("tablinks");
-            for (i = 0; i < tablinks.length; i++) {
-               tablinks[i].className = tablinks[i].className.replace(" active", "");
-            }
-            document.getElementById(cityName).style.display = "block";
-            evt.currentTarget.className += " active";
-         }
-         // Get the element with id="defaultOpen" and click on it
-         document.getElementById("defaultOpen").click();
-      </script>
 
 
    <?php } ?>
 
-   <div class="floating-button"  data-bs-toggle="modal" data-bs-target="#exampleModal">
+   <div class="floating-button">
       <a href="#" class="material-icons"><i class="fa fa-plus"></i></a>
-    
-
-
-
+   </div>
    </div>
 
 </div>
+
